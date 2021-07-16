58b79c9681870833e1aa2639778b1975df8e3e06
gromov-nikita <hromay.n999@gmail.com>
2021-07-17 01:43:56 +0300

    1

---
+++ 35)/src/Runner.java
@@ -0,0 +1,7 @@
+/*
+Exercise 35: (8) Modify BankTellerSimulation.java so that it represents Web clients
+making requests of a fixed number of servers. The goal is to determine the load that the
+group of servers can handle.
+ */
+public class Runner {
+}
